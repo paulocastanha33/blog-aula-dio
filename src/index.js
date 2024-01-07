67fbf949aa96d5e20333bc50a3dc31af66d1633b
paulocastanha <p33castanha@gmail.com>
2024-01-06 21:44:50 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const http = require("http")
+
+const hostname ="localhost"
+const port = 3333
+
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const router = express.Router()
+
+router.get("/",(req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/home.html"))
+})
+
+router.get("/contato",(req, res)=>{
+    res.sendFile(path.join(__dirname + "/pages/contato.html"))
+})
+
+app.use(router)
+
+app.listen(3333, ()=>{
+    console.log("SERVIDOR RODANDO....")
+})
+
